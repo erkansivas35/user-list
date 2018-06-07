255d48788611114e0794ea87a74c31970091376b
Erkan Sivas <erkan.sivas35@gmail.com>
2018-06-07 14:38:11 +0000

    Revert "bug fixes"
    This reverts commit 2c148db406bff3d9474268bc082f2794a632a40e

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,34 +1,25 @@
-<template>  
-  <div class="user-list"> 
-      <p v-if="loading" class="m-set">Loading...</p>
-      <div class="user" v-for="(user, index) in userList" :key="index">
+<template>   
+      <div class="user">
         <div class="user-img">
           <img :src="user.avatar" alt="User Name">
         </div>
 
         <div class="user-details">
-          <router-link :to="/user-detail/ + user.id"><p>{{ user.first_name }} {{ user.last_name }}</p></router-link>
+          <router-link :to="userLink"><p>{{ user.first_name }} {{ user.last_name }}</p></router-link>
         </div>
       </div>
-  </div>
 </template>
 
 <script>
 export default {
   name: "Users",
-  data() {
-    return {
-      loading: true,
-      userList: []
-    };
+  props: {
+    user: { type: Object, required: true }
   },
-  created() {
-    this.axios
-      .get("https://reqres.in/api/users?page=1&per_page=20")
-      .then(response => {
-        this.userList = response.data.data;
-        this.loading = false;
-      });
+  computed: {
+      userLink() {
+        return `/user-detail/${this.user.id}`
+      }
   }
 };
 </script>
